438f72ad8e70b29333c6a03e724fc6c543db65ba
rosella02 <roselayuanita29@gmail.com>
2019-08-31 13:11:01 +0700

    roselayuanita

---
+++ MyApplication/app/src/main/java/com/example/myapplication/HOME/fragmentlogin.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.HOME
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class fragmentlogin : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.activity_login, container, false)
+}
